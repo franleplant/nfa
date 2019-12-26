bd81fa210338ee1791918ca4166a2c93f6d056c9
Fran Guijarro <franleplant@gmail.com>
2019-12-26 12:16:05 -0300

    backup

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,54 @@
 //}
 use std::collections::HashMap;
 
+//trait States {
+    //fn get_states(&self) -> Vec<u32>;
+//}
+
+//impl States for u32 {
+    //fn get_states(&self) -> Vec<u32> {
+        //vec![self.clone()]
+    //}
+//}
+
+//impl<'a> States for &'a [u32] {
+    //fn get_states(&self) -> Vec<u32> {
+        //self.to_vec()
+    //}
+//}
+
+trait Transition {
+    fn get_current_state(&self) -> u32;
+    fn get_input(&self) -> String;
+    fn get_next_states(&self) -> Vec<u32>;
+}
+
+impl<'a> Transition for (u32, &'a str, u32) {
+    fn get_current_state(&self) -> u32 {
+        self.0.clone()
+    }
+    fn get_input(&self) -> String {
+        self.1.to_string()
+    }
+    fn get_next_states(&self) -> Vec<u32> {
+        vec![self.2.clone()]
+    }
+}
+
+impl<'a, 'b> Transition for (u32, &'a str, &'b[u32]) {
+    fn get_current_state(&self) -> u32 {
+        self.0.clone()
+    }
+    fn get_input(&self) -> String {
+        self.1.to_string()
+    }
+    fn get_next_states(&self) -> Vec<u32> {
+        self.2.to_vec()
+    }
+}
+
+
+
 //example https://en.wikipedia.org/wiki/Nondeterministic_finite_automaton
 
 struct NFA {
@@ -32,13 +80,11 @@ struct NFA {
 }
 
 impl NFA {
-    fn new<A>(final_states: &[u32], transitions: &[(u32, A, Vec<u32>)]) -> NFA
-    where
-        A: Into<String> + Copy,
+    fn new(final_states: &[u32], transitions: &[Box<Transition>]) -> NFA
     {
         let mut map = HashMap::new();
-        for &(state, symbol, ref next_states) in transitions {
-            map.insert((state, symbol.into()), next_states.clone());
+        for t in transitions {
+            map.insert((t.get_current_state(), t.get_input()), t.get_next_states());
         }
 
         return NFA {
@@ -49,7 +95,7 @@ impl NFA {
         };
     }
 
-    fn reset_State(&mut self) {
+    fn reset_state(&mut self) {
         // TODO closure of this state
         self.state = vec![0]
     }
@@ -83,14 +129,14 @@ impl NFA {
     }
 
 
-    fn consume_string(&self, input: &String) -> bool {
-        self.reset_state();
-        for symbol in input.chars() {
-            self.consume(symbol)
-        }
+    //fn consume_string(&mut self, input: &String) -> bool {
+        //self.reset_state();
+        //for symbol in input.chars() {
+            //self.consume(&symbol.to_string());
+        //}
 
-        return self.is_accepted();
-    }
+        //return self.is_accepted();
+    //}
 }
 
 #[cfg(test)]
@@ -98,18 +144,23 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
+        //let transitions: &[(u32, &str, &States)] =    &[
+                //(0, "", &[0, 1]),
+                //(1, "0", &2),
+                ////(1, "1", 1),
+        //];
         let m = NFA::new(
             &[1, 3],
             &[
-                (0, "", vec![0, 1]),
-                (1, "0", vec![2]),
-                (1, "1", vec![1]),
-                (2, "0", vec![1]),
-                (2, "1", vec![2]),
-                (3, "0", vec![3]),
-                (3, "1", vec![4]),
-                (4, "0", vec![4]),
-                (4, "1", vec![3]),
+                Box::new((0, "", &[0, 1])),
+                Box::new((1, "0", &2)),
+                //(1, "1", 1),
+                //(2, "0", 1),
+                //(2, "1", 2),
+                //(3, "0", 3),
+                //(3, "1", 4),
+                //(4, "0", 4),
+                //(4, "1", 3),
             ],
         );
         assert_eq!(2 + 2, 4);
